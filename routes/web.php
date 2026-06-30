f24d65d52fd5b7e03ac46a1f43b238eb7258ec3b
Fvdliii <fgunawan528@gmail.com>
2026-06-30 22:02:46 +0800

    pertemuan 11: layouting

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard.index');
+Route::resource('user', UserController::class);
